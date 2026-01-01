1f8780f998266c9d20c0e8469be22683c36b6773
Jeffrey Hartman <jeffreydavidhartman@gmail.com>
2026-01-01 15:46:44 -0500

    Initial commit - menu skeleton

---
+++ src/SoloForge.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+
+while (true)
+{
+    AnsiConsole.Clear();
+
+    var choice = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+            .Title("What do you want to do?")
+            .AddChoices(new[] { "Fate Check", "Random Event", "NPC Generator", "Dice Roller", "Settings", "Exit" })   
+    );
+
+    if (choice == "Exit")
+    {
+        AnsiConsole.WriteLine("Goodbye!");
+        break;
+    }
+    else
+    {
+        AnsiConsole.WriteLine("Not implemented yet...");
+        AnsiConsole.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
